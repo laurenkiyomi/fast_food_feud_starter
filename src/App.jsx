b6ad253e63fc6d5047dd3902edcc5bc6da455382
Lauren Lee <86620096+laurenkiyomi@users.noreply.github.com>
2022-06-20 13:24:34 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from "./components/Chip/Chip"
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
 
 // don't move this!
 export const appInfo = {
@@ -21,37 +25,93 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+  const [selectedCategory, setCategory] = React.useState("")
+  const [selectedRestaurant, setRestaurant] = React.useState("")
+  const [selectedMenuItem, setMenuItem] = React.useState(null)
+  const [instruction, setInstruction] = React.useState(appInfo.instructions.start)
+
+  const currentMenuItems = data.filter((item) => {
+    return item.food_category === selectedCategory && item.restaurant === selectedRestaurant
+  })
+    
+
+  const selectCategory = (category) => {
+    if (selectedCategory == "" && selectedRestaurant == "") {
+      setInstruction(appInfo.instructions.onlyCategory)
+    }
+
+    else {
+      setInstruction(appInfo.instructions.noSelectedItem)
+    }
+    setMenuItem(null)
+    setCategory(category)
+
+  }
+
+  const selectRestaurant = (restaurant) => {
+    if (selectedCategory == "" && selectedRestaurant == "") {
+      setInstruction(appInfo.instructions.onlyRestaurant)
+    }
+
+    else {
+      setInstruction(appInfo.instructions.noSelectedItem)
+    }
+    setMenuItem(null)
+    setRestaurant(restaurant)
+  }
+
+  const selectMenuItem = (menuItem) => {
+    setInstruction(appInfo.instructions.allSelected)
+    setMenuItem(menuItem)
+  }
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
       <div className="CategoriesColumn col">
         <div className="categories options">
           <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
+          {categories.map((category) => {
+            return(
+            <Chip key={category} label={category} onClick={() => selectCategory(category)}
+            isActive = {selectedCategory === category}/>)
+          })}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
-
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description}/>
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {restaurants.map((restaurant) => {
+              return (
+            <Chip key={restaurant} label={restaurant} onClick={() => selectRestaurant(restaurant)}
+            isActive = {selectedRestaurant === restaurant}/>)
+            })}
+          </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
-
+        <Instructions instructions={instruction}/>
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
+            {currentMenuItems.map((menuItem) => {
+              return (
+                <Chip key={menuItem.item_name} label={menuItem.item_name} onClick={() => selectMenuItem(menuItem)}
+                isActive = {selectedMenuItem === menuItem}/>)
+            })}  
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+           {selectedMenuItem ? <NutritionalLabel item={selectedMenuItem}/> : null}
+          </div>
         </div>
 
         <div className="data-sources">
